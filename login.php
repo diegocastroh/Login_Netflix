44153e6a9ead00abfc14833eceb1dc29b46e90ce
Diego Castro <lol_2015_2016@outlook.es>
2022-10-25 11:13:33 -0500

    Login actualizado y finalizado
    Falta conexión a PHP y una base de datos a usuario a testear

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/login_styles.css">
+</head>
+<body>
+    <nav class="navbar navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" style="color: white;" href="#">
+            <img src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+            Bootstrap
+            </a>
+        </div>
+    </nav>
+    <div class="m-0 row justify-content-center align-items-center" style="width: 100%; height: 90%">
+        <!-- Usuario -->
+        <div class="col-auto">
+            <!-- Estructura de un Item/Producto -->
+            <div class="card" style="width: 15rem; min-width: 250px; background: rgba(30,30,30,10);">
+                <img src="img/gato.webp" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title" style="text-align: center; color:white">Card title</h5>
+                <button href="#" class="btn btn-success" style="width: 100%;"><img src="img/image.png" alt="" width="20" height="20" class="d-inline-block align-text-top"> Añadir</button>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    
+</body>
+</html>
+
+<?php
+
+?>
