a6081c615e1e303afce787a0c4f36b36d668b443
Diego Castro <lol_2015_2016@outlook.es>
2022-10-27 01:49:10 -0500

    Implementación de CRUD, uso conectado a BD
    CRUD a base de datos consulta de USUARIOS

---
+++ Views/login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/login_styles.css">
+</head>
+<body>
+    <nav class="navbar navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" style="color: white;" href="#">
+            <img src="../img/Logo.png" alt="" width="30" height=auto class="d-inline-block align-text-top">
+            DesCode
+            </a>
+        </div>
+    </nav>
+    <!-- Contenedor Usuarios -->
+    <div class="m-0 row justify-content-center align-items-center" style="width: 100%; height: 90%">
+
+        <h1 style="text-align: center; color:white">¿Quien eres?</h1>
+        <!-- Instancia del conector -->
+        <?php
+            include_once("../Models/config.php");
+            $sql = "SELECT id, usuario, icono, rol FROM usuarios"; /* Consulta SQL */
+            $query = mysqli_query($conexion, $sql);
+            while($users = mysqli_fetch_array($query)){?>
+                <!-- Usuario -->
+                <div class="col-auto">
+                    <!-- Estructura de un Item/Producto -->
+                    <div class="card" style="width: 15rem; min-width: 250px; background: black;">
+                        <img src="../img/perfil_icons/<?php echo $users['icono']?>" class="card-img-top" alt="...">
+                        <div class="card-body" style="text-align: center; color:white">
+                            <h4 class="card-title" ><?php echo $users['usuario']?></h4>
+                            <?php
+                            $rol = $users['rol'];
+                             if ($rol === "Administrador"){
+                                $color = "#fe0000";
+                            }else{
+                                $color = "#51BCFF";
+                            }
+                            ?>
+                            <p class="card-text texto-borde" style="color: <?php echo $color?>;"><?php echo $rol?></p>
+                            <button href="#" class="btn btn-secondary" style="width: 100%">Iniciar Sesion</button>
+                        </div>
+                    </div>
+                </div>
+        <?php } ?>
+
+        <!-- Botón de registro de usuario -->
+        <div class="m-0 row justify-content-center">
+            <a href="../Views/registro.php" class="link-light" style="width: 100%; text-align: center;">Registrarse</a>
+        </div>
+    </div>
+</body>
+</html>
+
